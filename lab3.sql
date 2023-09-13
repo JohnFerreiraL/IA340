bbe9647488e0e473a24e941e15450356b83f81ae
JohnFerreiraL <143443612+JohnFerreiraL@users.noreply.github.com>
2023-09-13 15:51:57 -0400

    lab3.sql

--- lab3.sql
+++ lab3.sql
@@ -14,7 +14,7 @@ where s_email = 's1@jmu.edu'
 --Q2.2
 insert into professor (p_email, p_name, office)
 values ('p1@jmu.edu','p1','o1'),
-	('p2@jmu.edu','p2','02');
+	('p2@jmu.edu','p2','o2');
 
 --Q2.3
 insert into course(c_number,c_name,room,p_email)
