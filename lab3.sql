31a11b767f60ede3bdaedbabacfe040f6ed578bf
JohnFerreiraL <143443612+JohnFerreiraL@users.noreply.github.com>
2023-09-13 16:09:34 -0400

    Update lab3.sql

--- lab3.sql
+++ lab3.sql
@@ -30,3 +30,14 @@ values ('s1@jmu.edu','c1'),
 	('s4@jmu.edu', 'c2'),
 	('s2@jmu.edu', 'c3'),
 	('s3@jmu.edu', 'c3');
+
+--Q2.5
+insert into professor (p_email, p_name, office)
+values ('p3@jmu.edu', 'p3', 'o3')
+
+insert into course (c_number, c_name, room, p_email)
+values ('c4', 'facebook', 'r1','p3@jmu.edu')
+
+--professor must go first because the professor email is being refrenced in the new course table
+
+--Q2.6
